5bfaa6b880c4819bed4883da92ea54b5384472d9
Matthias <thisancog@gmail.com>
2016-08-01 14:55:39 +0200

    Update dislike-button.js

--- dislike-button/lib/dislike-button.js
+++ dislike-button/lib/dislike-button.js
@@ -3,8 +3,8 @@ var dislike_button_active = false;
 jQuery(document).ready(function() {
 	jQuery('.dislike-button').each(function() {
 		var id = jQuery(this).attr('id').substr(15);
-
-		var cookie = (jQuery(this).hasClass('default')) ? 'default' : 'active';
+		
+		var cookie = (jQuery(this).hasClass('active')) ? 'active' : 'default';
 		var cname = "dislike-" + id + "=";
     	var ca = document.cookie.split(';');
     	for (var i = 0; i < ca.length; i++) {
@@ -25,34 +25,40 @@ jQuery(document).ready(function() {
 		},
 		elem = this;
 	
-		jQuery.post(dislike_ajax_url, data, function(response) {
-			dislike_button_update(elem, response);
-		});
+		dislike_button_update(elem, data);
 	});
 
 	jQuery('.dislike-button').click(function() {
 		if (!dislike_button_active) {
 			dislike_button_active = true;
+			var query = (jQuery(this).hasClass('default')) ? 'dislike' : 'undo';
 			var data = {
 				'action':	'update_dislike_button',
 				'id':		jQuery(this).attr('id').substr(15),
-				'query':	(jQuery(this).hasClass('default')) ? 'dislike' : 'undo',
+				'query':	query,
 				'state': 	"update" 
 			}, elem = this;
 	
-			jQuery.post(dislike_ajax_url, data, function(response) {
-				dislike_button_update(elem, response);			
-			});
+			dislike_button_update(elem, data);
 		}
 	});
 });
 
-function dislike_button_update(e, response) {
-	jQuery(e).removeClass('default active').addClass(response.state);
-	jQuery(e).find('.dislike-button-text').html(eval('dislike_' + response.state));
-	jQuery(e).find('.dislike-button-count').html(response.count);
-	var expiry = new Date();
-	expiry.setTime(expiry.getTime()+(99*365*24*60*60*1000)); 
-	document.cookie = "dislike-" + response.id + "=" + response.state + "; expires=" + expiry.toGMTString();
-	dislike_button_active = false;
+function dislike_button_update(elem, data) {
+	jQuery.ajax({
+		url: dislike_ajax_url,
+		type: 'POST',
+		data: data,
+		success: function(response) {
+			console.log('response: ' + response);
+			var state = (response.state) ? response.state : 'default';
+			jQuery(elem).removeClass('default active').addClass(state);
+			jQuery(elem).find('.dislike-button-text').html(eval('dislike_' + state));
+			jQuery(elem).find('.dislike-button-count').html(response.count);
+			var expiry = new Date();
+			expiry.setTime(expiry.getTime()+(99*365*24*60*60*1000)); 
+			document.cookie = "dislike-" + response.id + "=" + state + "; expires=" + expiry.toGMTString();
+			dislike_button_active = false;
+		}
+	});
 }
