8544852cbbbb2cdfb32de30197631f89020feab3
Matthias <thisancog@gmail.com>
2016-08-01 02:05:19 +0200

    Update dislike-button.js

--- dislike-button/lib/dislike-button.js
+++ dislike-button/lib/dislike-button.js
@@ -23,7 +23,7 @@ jQuery(document).ready(function() {
 		},
 		elem = this;
 	
-		jQuery.post(ajax_url, data, function(response) {
+		jQuery.post(dislike_ajax_url, data, function(response) {
 			dislike_button_update(elem, response);
 		});
 	});
@@ -35,7 +35,7 @@ jQuery(document).ready(function() {
 			'query':	(jQuery(this).hasClass('default')) ? 'dislike' : 'undo'
 		}, elem = this;
 	
-		jQuery.post(ajax_url, data, function(response) {
+		jQuery.post(dislike_ajax_url, data, function(response) {
 			dislike_button_update(elem, response);			
 		});
 	});
@@ -43,6 +43,7 @@ jQuery(document).ready(function() {
 
 function dislike_button_update(e, response) {
 	jQuery(e).removeClass('default active').addClass(response.state);
+	jQuery(e).find('.dislike-button-text').html(eval('dislike_' + response.state));
 	jQuery(e).find('.dislike-button-count').html(response.count);
 	var expiry = new Date();
 	expiry.setTime(expiry.getTime()+(99*365*24*60*60*1000)); 
