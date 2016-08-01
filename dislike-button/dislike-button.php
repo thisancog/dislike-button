d1dcb3d133f6e1d7d16a4fbf1fbca6c47ddeedaa
Matthias <thisancog@gmail.com>
2016-08-01 02:46:16 +0200

    Update dislike-button.php

--- dislike-button/dislike-button.php
+++ dislike-button/dislike-button.php
@@ -8,9 +8,8 @@ Author: Matthias Planitzer
 
 /******************************************************************************************
 	
-	If I had a penny for every client who wants a like button for his website but would
-	probably need a dislike button even more...
 	This is a simple dislike button plugin for Wordpress.
+	Because many websites and oblivious editors would benefit from it. 
 
 	Copy the folder to your /wp_content/plugins folder, activate the plugin in the Wordpress
 	backend and include the PHP snippet <?php dislike_button(); ?> where the button should
